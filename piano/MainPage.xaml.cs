9d88b62dc8b64e1ef14e16b9683d8b4e823f70dd
Alice Jin <t-zhjin@microsoft.com>
2016-06-07 15:44:42 -0700

    c button works!!!!

--- piano/MainPage.xaml.cs
+++ piano/MainPage.xaml.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using org.allseen.LSF.LampState;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Devices.AllJoyn;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,55 +24,131 @@ namespace piano
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        MediaElement beep;
-
+        MediaElement A, B, B_b, C, C_s, D, E, E_b, F, F_s, G, G_s;
+        uint[] colors;
+        AllJoynBusAttachment busAttachment = null;
+        String lampId = "d3cc1d629dda8d3df81fe0129bc2bf56";
+        LampStateConsumer consumer = null;
 
         public MainPage()
         {
             this.InitializeComponent();
-            beep = new MediaElement();
-            this.setMedia();
+
+            this.setAllMedia();
+
+            colors = new uint[] { 0, 34, 51, 101, 153, 167, 204 };
+            for (int i = 0; i < 7; i++)
+            {
+                colors[i] = (uint)colors[i] * 14035841;
+            }
+
+            busAttachment = new AllJoynBusAttachment();
+            LampStateWatcher watcher = new LampStateWatcher(busAttachment);
+            watcher.Added += Watcher_Added; 
+
+            watcher.Start();
         }
 
-        private async void setMedia()
+        private async void setAllMedia()
         {
+            A = new MediaElement();
+            B = new MediaElement();
+            B_b = new MediaElement();
+            C = new MediaElement();
+            C_s = new MediaElement();
+            D = new MediaElement();
+            E = new MediaElement();
+            E_b = new MediaElement();
+            F = new MediaElement();
+            F_s = new MediaElement();
+            G = new MediaElement();
+            G_s = new MediaElement();
             Windows.Storage.StorageFolder folder = await Windows.ApplicationModel.Package.Current.InstalledLocation.GetFolderAsync("Assets");
-            Windows.Storage.StorageFile file = await folder.GetFileAsync("beep.wav");
-            var stream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read);
-            beep.SetSource(stream, file.ContentType);
+            Windows.Storage.StorageFile file = await folder.GetFileAsync("A.wav");
+            A.SetSource(await file.OpenAsync(Windows.Storage.FileAccessMode.Read), file.ContentType);
+            file = await folder.GetFileAsync("B.wav");
+            B.SetSource(await file.OpenAsync(Windows.Storage.FileAccessMode.Read), file.ContentType);
+            file = await folder.GetFileAsync("B_b.wav");
+            B_b.SetSource(await file.OpenAsync(Windows.Storage.FileAccessMode.Read), file.ContentType);
+            file = await folder.GetFileAsync("C.wav");
+            C.SetSource(await file.OpenAsync(Windows.Storage.FileAccessMode.Read), file.ContentType);
+            file = await folder.GetFileAsync("C_#.wav");
+            C_s.SetSource(await file.OpenAsync(Windows.Storage.FileAccessMode.Read), file.ContentType);
+            file = await folder.GetFileAsync("D.wav");
+            D.SetSource(await file.OpenAsync(Windows.Storage.FileAccessMode.Read), file.ContentType);
+            file = await folder.GetFileAsync("E.wav");
+            E.SetSource(await file.OpenAsync(Windows.Storage.FileAccessMode.Read), file.ContentType);
+            file = await folder.GetFileAsync("E_b.wav");
+            E_b.SetSource(await file.OpenAsync(Windows.Storage.FileAccessMode.Read), file.ContentType);
+            file = await folder.GetFileAsync("F.wav");
+            F.SetSource(await file.OpenAsync(Windows.Storage.FileAccessMode.Read), file.ContentType);
+            file = await folder.GetFileAsync("F_#.wav");
+            F_s.SetSource(await file.OpenAsync(Windows.Storage.FileAccessMode.Read), file.ContentType);
+            file = await folder.GetFileAsync("G.wav");
+            G.SetSource(await file.OpenAsync(Windows.Storage.FileAccessMode.Read), file.ContentType);
+            file = await folder.GetFileAsync("G_#.wav");
+            G_s.SetSource(await file.OpenAsync(Windows.Storage.FileAccessMode.Read), file.ContentType);
         }
 
-        private void button_C(object sender, RoutedEventArgs e)
+        private async void Watcher_Added(LampStateWatcher sender, AllJoynServiceInfo args) // can only wait on async method inside async method
         {
-            beep.Play();
+            AllJoynAboutDataView aboutData = await AllJoynAboutDataView.GetDataBySessionPortAsync(args.UniqueName, busAttachment, args.SessionPort);
+            if (aboutData != null && !string.IsNullOrWhiteSpace(aboutData.DeviceId) && string.Equals(aboutData.DeviceId, lampId))
+            {
+                LampStateJoinSessionResult result = await LampStateConsumer.JoinSessionAsync(args, sender);
+                if (result.Status == AllJoynStatus.Ok)
+                {
+                    consumer = result.Consumer;
+                }
+            }
         }
 
-        private void button_D(object sender, RoutedEventArgs e)
+        private async void turn_off()
+        {
+            if (consumer != null)
+                await consumer.SetOnOffAsync(false);
+        }
+
+        private async void c_button_click(object sender, RoutedEventArgs e)
+        {
+            if (consumer != null)
+            {
+                await consumer.SetOnOffAsync(true);
+                await consumer.SetHueAsync(colors[0]);
+                if (!c_button.IsPressed)
+                {
+                    turn_off();
+                }
+            }
+            C.Play();
+        }
+
+        private void d_button_click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void button_E(object sender, RoutedEventArgs e)
+        private void e_button_click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void button_F(object sender, RoutedEventArgs e)
+        private void f_button_click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void button_G(object sender, RoutedEventArgs e)
+        private void g_button_click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void button_A(object sender, RoutedEventArgs e)
+        private void a_button_click(object sender, RoutedEventArgs e)
         {
 
         }
 
-        private void button_B(object sender, RoutedEventArgs e)
+        private void b_button_click(object sender, RoutedEventArgs e)
         {
 
         }
